da6c6c3034de755a231fc447d9b296355285bd22
jirka prihoda <jirkaprihoda@gmail.com>
2017-04-25 13:57:48 +0200

    gitignore update, scss files, index structure

---
+++ wp-content/themes/modely/gulpfile.js
@@ -0,0 +1,101 @@
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-cssnano gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
+
+// Styles
+gulp.task('styles', function() {
+  return sass('src/styles/*.scss', { style: 'expanded' , sourcemap: true})
+    .on('error', sass.logError)
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/styles'))
+     // for inline sourcemaps 
+    .pipe(sourcemaps.write())
+        // for file sourcemaps 
+    .pipe(sourcemaps.write('/', {
+        includeContent: false,
+        sourceRoot: 'source'
+    }))
+    .pipe(gulp.dest('dist/styles'));
+});
+
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/styles', 'dist/scripts', 'dist/images']);
+});
+
+// Watch
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }),
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+
+    gulp.watch(['dist/**']).on("change", reload);
+    gulp.watch(['src/**']).on("change", reload);
+
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'images');
+});
+
+// Serve task
+gulp.task('serve', ['clean'], function() {
+  gulp.start('styles', 'scripts', 'images', 'watch' );
+});
